9491979300fbaf1215fbc57e6957f5e099c2f2bc
İsmahan Arı <ismhnarii@gmail.com>
2023-09-01 18:01:39 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoApp';
-}
+  user = 'İsmahan';
+  items = [
+    { description: "Kahvaltı", action: "No" },
+    { description: "sinama", action: "No" },
+    { description: "spor", action: "No" },
+    { description: "Ders çalışma", action: "No" }
+  ] ; }
